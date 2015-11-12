ea73136b847ab935517d047bb5fb7bb4bbdb17a3
Jörgen Brandt <joergen.brandt@onlinehome.de>
2015-11-12 16:37:04 +0100

    Attributes adapted.

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,4 +4,3 @@ default.dir.data = "/opt/data"
 default.kgenomes.dirname = "kgenomes"
 default.kgenomes.idlist = ["359188", "359195"]
 
-default.dir.kgenomes = "#{default.dir.data}/#{default.kgenomes.dirname}"
