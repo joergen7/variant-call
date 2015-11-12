6b091b0819a95ed7960f4cfe0ca7cb138e8c7f28
Jörgen Brandt <joergen.brandt@onlinehome.de>
2015-11-12 16:35:29 +0100

    Data directory now set in recipe instead of default attribute.

--- recipes/data.rb
+++ recipes/data.rb
@@ -6,12 +6,13 @@
 # Copyright (c) 2015 Jörgen Brandt, All Rights Reserved.
 
 kgenomes_url_base = "ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/data/HG02025/sequence_read/SRR"
+kgenomes_dir = "#{node.dir.data}/#{node.kgenomes.dirname}"
 
 include_recipe "chef-bioinf-worker::hg38"
 
 # create directories
 directory node.dir.data
-directory node.dir.kgenomes
+directory kgenomes_dir
 
 
 # download read data
@@ -20,13 +21,13 @@ node.kgenomes.idlist.each { |id|
   url1 = "#{kgenomes_url_base}#{id}_1.filt.fastq.gz"
   url2 = "#{kgenomes_url_base}#{id}_2.filt.fastq.gz"
 	
-  remote_file "#{node.dir.kgenomes}/#{File.basename( url1 )}" do
+  remote_file "#{kgenomes_dir}/#{File.basename( url1 )}" do
     action :create_if_missing
     source url1
     retries 1
   end
   
-  remote_file "#{node.dir.kgenomes}/#{File.basename( url2 )}" do
+  remote_file "#{kgenomes_dir}/#{File.basename( url2 )}" do
     action :create_if_missing
     source url2
     retries 1
