15c1c2d8aca4feb6e358553f1148c5bad3bbb9e1
Jorgen Brandt <joergen.brandt@onlinehome.de>
2017-08-03 18:42:52 +0200

    attributes file fixed.

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,5 +2,5 @@ default["dir"]["wf"] = "/opt/wf"
 default["dir"]["data"] = "/opt/data"
 
 default["kgenomes"]["dirname"] = "kgenomes"
-default["kgenomes"]["dirname"] = [359188, 359195]
+default["kgenomes"]["idlist"] = [359188, 359195]
 
