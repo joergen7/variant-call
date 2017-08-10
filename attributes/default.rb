6b37b17a54fafc75f9c0f054a468bc0564ac9acd
Jorgen Brandt <joergen.brandt@onlinehome.de>
2017-08-10 11:58:26 +0200

    HG38 added to data recipe.

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,3 +4,32 @@ default["dir"]["data"] = "/opt/data"
 default["kgenomes"]["dirname"] = "kgenomes"
 default["kgenomes"]["idlist"] = [359188, 359195]
 
+default["hg38"]["dirname"] = "hg38"
+default["hg38"]["idlarge"] = [
+"chr1",
+"chr2",
+"chr3",
+"chr4",
+"chr5",
+"chr6",
+"chr7",
+"chr8",
+"chr9",
+"chr10",
+"chr11",
+"chr12",
+"chr13",
+"chr14",
+"chr15",
+"chr16",
+"chr17",
+"chr18",
+"chr19",
+"chr20",
+"chr21",
+"chr22",
+"chrX",
+"chrY"
+]
+default["hg38"]["smallname"] = "small.fa"
+default["hg38"]["fullname"] = "hg38.tar"
