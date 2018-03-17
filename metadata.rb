b4ec90ef1692f983dc4962a4a66daf8c848b51f3
Jörgen <joergen.brandt@onlinehome.de>
2018-03-17 11:26:41 +0100

    Fix versions of cookbooks this cookbook depends on

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "brandjoe@hu-berlin.de"
 license 'all_rights'
 description 'Installs/Configures variant-call'
 long_description 'Installs/Configures variant-call'
-version '0.1.0'
+version '0.1.1'
 
 depends "chef-bioinf-worker", "0.1.0"
-depends "chef-cuneiform", "0.1.0"
+depends "chef-cuneiform", "0.1.1"
