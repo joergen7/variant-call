a247e184d2e9ab80bf69c8a46f1dd543423a3fe0
Jorgen Brandt <joergen.brandt@onlinehome.de>
2018-12-17 07:40:55 +0100

    Workflow updated, version set to 0.1.3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'joergen.brandt@onlinehome.de'
 license          'Apache 2.0'
 description      'Installs/Configures variant-call'
 long_description 'Installs/Configures variant-call'
-version          '0.1.2'
+version          '0.1.3'
 chef_version     '>= 12.14' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
@@ -19,4 +19,4 @@ issues_url 'https://github.com/joergen7/variant-call/issues'
 #
 source_url 'https://github.com/joergen7/viariant-call'
 
-depends "chef-cuneiform", "0.1.4"
+depends 'chef-cuneiform', '0.1.4'
